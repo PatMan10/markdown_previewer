8acb1f2e3cc4d6de705fc65b0f0018d1ed886a9c
PatMan10 <PatMan10@protonmail.com>
2020-01-06 18:55:10 +0200

    cleaned up

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+//--------CSS----------------
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "pmt-kickstart.css/src/kickstart.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
+//--------LIBS----------------
+import React from "react";
+//--------UI_COMP----------------
+
+const App: React.FC = () => {
+  return (
+    <main className="red-bg flex-row-aiC-jcC">
+      <h1>Mark Down Previewer</h1>
+    </main>
+  );
+};
+
+export default App;
